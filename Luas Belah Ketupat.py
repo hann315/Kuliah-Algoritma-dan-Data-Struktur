74c14825375914a550f7c04aba9dcab63946318f
hann315 <64533786+hann315@users.noreply.github.com>
2022-05-31 14:44:31 +0700

    Update Luas Belah Ketupat.py

--- Luas Belah Ketupat.py
+++ Luas Belah Ketupat.py
@@ -1,8 +1,8 @@
-print("Nama: Faisal A. F. Rahman\nNIM: 2130511027")
+print("Nama: Faisal A. F. Rahman\nNIM: 2130511027\n")
 
 def cari_luas_belahketupat(p, q):  
     return ( ( p * q ) / 2)
 p = float(input("Masukkan panjang Diagonal 1: "))
 q = float(input("Masukkan panjang Diagonal 2: "))
 
-print("Luas dari belah ketupat dengan panjang", p, "cm dan", q, "cm adalah",cari_luas_belahketupat(p, q), "cm3")
+print("\nLuas dari belah ketupat dengan panjang", p, "cm dan", q, "cm adalah",cari_luas_belahketupat(p, q), "cm3")
