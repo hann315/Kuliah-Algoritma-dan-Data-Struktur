c745f85097beaff701d97746379374a6e057ee0b
hann315 <64533786+hann315@users.noreply.github.com>
2022-06-01 20:16:50 +0700

    Update Luas Belah Ketupat.py

--- Luas Belah Ketupat.py
+++ Luas Belah Ketupat.py
@@ -1,7 +1,18 @@
 print("Nama: Faisal A. F. Rahman\nNIM: 2130511027\n")
 
 def cari_luas_belahketupat(p, q):  
-    return ( ( p * q ) / 2)
+    """
+    Fungsi didefinisikan dengan keyword def diikuti dengan nama fungsi dan parameternya dalam kurung (). 
+    """
+
+    operasi_hitung = p * q / 2
+
+    """
+    Pernyataan return [expression] akan membuat eksekusi program keluar dari fungsi saat itu, sekaligus 
+    mengembalikan nilai tertentu. Nilai return yang tidak mengembalikan (ekspresi) nilai bersifat sama. 
+    """
+    return operasi_hitung
+    
 p = float(input("Masukkan panjang Diagonal 1: "))
 q = float(input("Masukkan lebar Diagonal 2: "))
 
